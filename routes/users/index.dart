778911901b94c60be78a77c95c62e986dce9a49c
bastndev <gohit24@gmail.com>
2024-01-03 10:29:49 -0500

    :arrow_up: Update `RECENT`

---
+++ routes/users/index.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+import 'package:dart_frog/dart_frog.dart';
+
+Response onRequest(RequestContext context) {
+  return Response(
+    body: jsonEncode({
+      'User': 'Gohit',
+    }),
+  );
+}
