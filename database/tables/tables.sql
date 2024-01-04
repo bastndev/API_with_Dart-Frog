2f15b2e03857447cd956115242e5b61580ec5816
bastndev <gohit24@gmail.com>
2024-01-03 22:18:12 -0500

    :arrow_up: Update `RECENT`

---
+++ database/tables/tables.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS protective;
+CREATE DATABASE protective ;
+use protective
+
+CREATE TABLE users (
+    id VARCHAR(255) PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE pets (
+    id VARCHAR(255) PRIMARY KEY,
+    adopted_by VARCHAR(255),
+    name VARCHAR(50) NOT NULL,
+    age int,
+    base64image longtext,
+    type VARCHAR(50) NOT NULL,
+    FOREIGN KEY (adopted_by) REFERENCES users(id)
+);
+
+CREATE TABLE access_tokens (
+    id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255),
+    token VARCHAR(500) NOT NULL,
+    expiration_date TIMESTAMP DEFAULT (NOW() + INTERVAL 2 HOUR),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
