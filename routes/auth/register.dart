d2782b5aced6bc7c9b6641e71a0d9330b063fef6
bastndev <gohit24@gmail.com>
2024-01-04 09:30:48 -0500

    :arrow_up: Update `RECENT`

--- routes/auth/register.dart
+++ routes/auth/register.dart
@@ -1,6 +1,14 @@
+import 'dart:convert';
+
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  // TODO: implement route handler
+  final method = context.request.method;
+  if (method != HttpMethod.post) {
+    return Response(
+      statusCode: 405,
+      body: jsonEncode({'error': 'Invalid method'}),
+    );
+  }
   return Response(body: 'This is a new route!');
 }
