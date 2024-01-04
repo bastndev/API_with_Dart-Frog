7a43741f34e55c1bbef322bc667b421aa0659ba7
bastndev <gohit24@gmail.com>
2024-01-04 18:38:28 -0500

    :arrow_up: Update `RECENT`

---
+++ exections/bad_reques.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:dart_frog/src/_internal.dart';
+
+import 'base_api_exeptions.dart';
+
+class BadRequestException implements BaseApiException {
+ final String message;
+  static const int statusCode = 400;
+  const BadRequestException(this.message);
+
+  @override
+  Response response() {
+    return Response(statusCode: statusCode, body: jsonEncode({'error' : message}));
+  }
+
+}
