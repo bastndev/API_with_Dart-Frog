5be366aec8dbd7cdb2e45b1f200c799d393f9511
bastndev <gohit24@gmail.com>
2024-01-05 16:13:56 -0500

    :arrow_up: Update `RECENT`

--- routes/pets/index.dart
+++ routes/pets/index.dart
@@ -8,29 +8,33 @@ import '../../repositories/pet_repository.dart';
 Future<Response> onRequest(RequestContext context) async {
   final method = context.request.method;
   if (method == HttpMethod.post) {
-    final body = await context.request.json();
-    if (body == null) {
-      return Response(
-        statusCode: 400,
-        body: jsonEncode({'error': 'incorrect request'}),
-      );
-    }
-
-    if (!_onPostPetValidation(body as Map<String, dynamic>)) {
-      return Response(
-        statusCode: 400,
-        body: jsonEncode({'error': 'Body petition incorrect'}),
-      );
-    }
-
-    final id = const Uuid().v4();
-    body['id'] = id;
-
-    final petRepository = context.read<PetRepository>();
-    await petRepository.create(body);
-    return Response(body: jsonEncode({'success': true}));
+    return _posPet(context);
   }
   return Response(body: 'Bajo construction');
+}
+
+Future<Response> _posPet(RequestContext context) async {
+  final body = await context.request.json();
+  if (body == null) {
+    return Response(
+      statusCode: 400,
+      body: jsonEncode({'error': 'incorrect request'}),
+    );
+  }
+
+  if (!_onPostPetValidation(body as Map<String, dynamic>)) {
+    return Response(
+      statusCode: 400,
+      body: jsonEncode({'error': 'Body petition incorrect'}),
+    );
+  }
+
+  final id = const Uuid().v4();
+  body['id'] = id;
+
+  final petRepository = context.read<PetRepository>();
+  await petRepository.create(body);
+  return Response(body: jsonEncode({'success': true}));
 }
 
 bool _onPostPetValidation(Map<String, dynamic> body) {
@@ -43,7 +47,7 @@ bool _onPostPetValidation(Map<String, dynamic> body) {
     }
     if (field == 'base64image' &&
         value is String &&
-        !value.contains('data:image')) {
+        !value.startsWith('data:image')) {
       return false;
     }
   }
