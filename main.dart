0a947d6bac66a13075c430b443a4e1c9196a5bb2
bastndev <gohit24@gmail.com>
2024-01-05 14:43:14 -0500

    :arrow_up: Update `RECENT`

--- main.dart
+++ main.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
-import 'dart:js';
+// import 'dart:js';
 
 import 'package:dart_frog/dart_frog.dart';
 
 import 'database/connection/database_client.dart';
+import 'repositories/auth_repository.dart';
+import 'repositories/user_repository.dart';
 import 'utils/jwt_manager.dart';
 
 Future<void> init(InternetAddress ip, int port) async {
@@ -12,7 +14,22 @@ Future<void> init(InternetAddress ip, int port) async {
 
 Future<HttpServer> run(Handler handler, InternetAddress ip, int port) {
   return serve(
-    handler.use(provider<JWTManager>((context) => const JWTManager('dart'))),
+    handler
+        .use(
+          provider<JWTManager>(
+            (context) => const JWTManager('dart'),
+          ),
+        )
+        .use(
+          provider<AuthRepository>(
+            (context) => AuthRepository(DatabaseClient.instance!),
+          ),
+        )
+        .use(
+          provider<UserRepository>(
+            (context) => UserRepository(DatabaseClient.instance!),
+          ),
+        ),
     ip,
     port,
   );
