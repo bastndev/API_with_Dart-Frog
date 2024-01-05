b53934dae6f422b7bfed1655028742abe8a991b7
bastndev <gohit24@gmail.com>
2024-01-05 15:12:41 -0500

    :arrow_up: Update `RECENT`

--- routes/pets/index.dart
+++ routes/pets/index.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:dart_frog/dart_frog.dart';
 
 
 Future<Response> onRequest(RequestContext context) async {
-  
-
+  final method = context.request.method;
+  if(method == HttpMethod.post){
+    return Response(body: jsonEncode({'success': true}));
+  }
+  return Response(body: 'Bajo construction');
 }
