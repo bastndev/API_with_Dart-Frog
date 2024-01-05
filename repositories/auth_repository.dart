173ab7c0200bab0e4779e9600aeb414ca0674ecb
bastndev <gohit24@gmail.com>
2024-01-04 20:40:28 -0500

    :arrow_up: Update `RECENT`

--- repositories/auth_repository.dart
+++ repositories/auth_repository.dart
@@ -11,4 +11,15 @@ class AuthRepository {
 
     await db.execute(sql, accessToken.toMap());
   }
+
+  Future<AccessToken> getByCredentials(String email, String password) async {
+    const sql =
+        'SELECT a.user_id, a.id, a.expiration_date, a.token from access_tokens a inner join users u on u.id = a.user_id'
+        ' where password = :password AND email = :email';
+
+    final results =
+        await db.execute(sql, {'email': email, 'password': password});
+    final jsonMap = results.rows.first.typedAssoc() as Map<String, dynamic>;
+    return AccessToken.fromMap(jsonMap);
+  }
 }
--- routes/auth/login.dart
+++ routes/auth/login.dart
@@ -1,6 +1,10 @@
 import 'dart:convert';
 
+import 'package:crypto/crypto.dart' as crypto;
 import 'package:dart_frog/dart_frog.dart';
+
+import '../../database/connection/database_client.dart';
+import '../../repositories/auth_repository.dart';
 
 Future <Response> onRequest(RequestContext context) async{
   final method = context.request.method;
@@ -11,15 +15,23 @@ Future <Response> onRequest(RequestContext context) async{
     );
   }
 
-  //1- get the body 
+  //1-TODO: get the body 
   final body = context.request.json() as Map<String, String>;
-  //2- verify the body 
 
-  //3- getByCredentials 
+  //2-TODO: verify the body 
+  if(!_isBodyValid(body)){
+    return Response(statusCode: 400, body: jsonEncode({'error': 'Invalid body'}));
+  }
 
-  //4- check token validity 
+  //3-TODO: getByCredentials 
+  final authRepo = AuthRepository(DatabaseClient.instance!);
+  final digest = crypto.sha1.convert(body['password'].toString().codeUnits);
+  await authRepo.getByCredentials(body['email']as String, body['password']as String);
+  body['password'] = digest.toString();
+  
+  //4-TODO: check token validity 
 
-  //5- return response
+  //5-TODO: return response
 
   return Response(body: 'This is a new route!');
 }
