5447dcafbe9ffbe12c0d9339401597bd0353000a
bastndev <gohit24@gmail.com>
2024-01-05 14:23:36 -0500

    :arrow_up: Update `RECENT`

--- utils/jwt_manager.dart
+++ utils/jwt_manager.dart
@@ -1,5 +1,12 @@
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 
+class JWTData {
+  final String token;
+  final DateTime expirationDate;
+
+  const JWTData(this.token, this.expirationDate);
+}
+
 class JWTManager {
   final String key;
 
