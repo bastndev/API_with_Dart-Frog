cdce41a8ed20a3459e18e27fe9c6ef08f48b0ae8
bastndev <gohit24@gmail.com>
2024-01-05 15:10:20 -0500

    :arrow_up: Update `RECENT`

--- routes/pets/index.dart
+++ routes/pets/index.dart
@@ -1,27 +1,7 @@
-import 'dart:convert';
-
 import 'package:dart_frog/dart_frog.dart';
-import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
-
-import '../../database/connection/database_client.dart';
-import '../../repositories/auth_repository.dart';
 
 
 Future<Response> onRequest(RequestContext context) async {
-  final headers = context.request.headers;
-  if (headers['Authorization'] == null) {
-    return Response(
-        statusCode: 401, body: jsonEncode({'error': 'Not authorized'}));
-  }
-  final authoRepo = AuthRepository(DatabaseClient.instance!);
-  print(headers['Authorization']);
-  final token = (headers['Authorization'] as String).split('Bearer')[1];
-  final  jwt =JWT.verify(token, SecretKey('dart'));
-  final uidMap = jwt.payload;
-  print(uidMap);
+  
 
-  // ignore: unused_local_variable
-  final accessToken = await authoRepo.getAccessByToken(token);
-
-  return Response(body: 'This is a new route!');
 }
