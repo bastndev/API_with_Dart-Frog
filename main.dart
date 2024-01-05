8dabefc186f5c7588a948e01514ffd9e51f84b4a
bastndev <gohit24@gmail.com>
2024-01-05 15:19:33 -0500

    :arrow_up: Update `RECENT`

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-// import 'dart:js';
+// backend use SQL
 
 import 'package:dart_frog/dart_frog.dart';
 
--- routes/pets/index.dart
+++ routes/pets/index.dart
@@ -2,10 +2,17 @@ import 'dart:convert';
 
 import 'package:dart_frog/dart_frog.dart';
 
-
 Future<Response> onRequest(RequestContext context) async {
   final method = context.request.method;
-  if(method == HttpMethod.post){
+  if (method == HttpMethod.post) {
+    final body = await context.request.json();
+    if (body == null) {
+      return Response(
+        statusCode: 400,
+        body: jsonEncode({'error': 'incorrect request'}),
+      );
+    }
+
     return Response(body: jsonEncode({'success': true}));
   }
   return Response(body: 'Bajo construction');
