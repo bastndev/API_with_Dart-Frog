4581bafdbb92b42217554e25133ef924ff3595ad
bastndev <gohit24@gmail.com>
2024-01-05 14:14:07 -0500

    :arrow_up: Update `RECENT`

--- middleware/auth_middleware.dart
+++ middleware/auth_middleware.dart
@@ -7,15 +7,22 @@ Middleware bearerAutorizationMiddleware() {
   return (handler) {
     return (context) {
       final headers = context.request.headers;
-      if (headers['Authorization'] == null || !headers['Authorization']!.startsWith('Bearer')) {
+      if (headers['Authorization'] == null ||
+          !headers['Authorization']!.startsWith('Bearer')) {
         return Response(
             statusCode: 401, body: jsonEncode({'error': 'Not authorized'}));
       }
       print(headers['Authorization']);
       final token = (headers['Authorization'])!.split('Bearer')[1];
-      final jwt = JWT.verify(token, SecretKey('dart'));
-      final uidMap = jwt.payload;
-      print(uidMap);
+      try {
+        final jwt = JWT.verify(token, SecretKey('dart'));
+        final uidMap = jwt.payload;
+        print(uidMap);
+      } catch (error) {
+        return Response(statusCode: 409,
+        body: jsonEncode({'error': 'Authorization no valid'})
+        );
+      }
 
       return handler(context);
     };
