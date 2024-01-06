6355c441667f90b16769482b43cd75c57200900c
bastndev <gohit24@gmail.com>
2024-01-06 15:53:02 -0500

    :arrow_up: Update `RECENT`

--- routes/pets/ws/adopted.dart
+++ routes/pets/ws/adopted.dart
@@ -1,7 +1,13 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
 
+Map<String, WebSocketChannel> clients = {};
+
 Future<Response> onRequest(RequestContext context) async {
+  final connectionInfo = context.request.connectionInfo;
+  final fullAddress =
+      '${connectionInfo.remoteAddress}:${connectionInfo.remotePort}';
+
   final handler = webSocketHandler((channel, protocol) {
     // TODO: react to new connections.
   });
