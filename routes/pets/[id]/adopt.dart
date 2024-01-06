bc9e070aaa9fe572a9b554a8f7e806ba28bf2237
bastndev <gohit24@gmail.com>
2024-01-06 14:41:16 -0500

    :arrow_up: Update `RECENT`

--- routes/pets/[id]/adopt.dart
+++ routes/pets/[id]/adopt.dart
@@ -1,12 +1,24 @@
+import 'dart:convert';
+
 import 'package:dart_frog/dart_frog.dart';
 
-Response onRequest(
+import '../../../exections/base_api_exeptions.dart';
+import '../../../repositories/pet_repository.dart';
+
+Future<Response> onRequest(
   RequestContext context,
   String id,
-) {
+) async {
   final userID = context.read<String>();
+  final petResponsitory = context.read<PetRepository>();
 
-
-
-  return Response(body: 'This is a nre rout!');
+  try {
+    await petResponsitory.adopt(id, userID);
+    return Response(body: jsonEncode({'success': true}));
+  } catch (err) {
+    if (err is BaseApiException) {
+      return err.response();
+    }
+    return Response(statusCode: 500, body: 'This is NOT rout');
+  }
 }
