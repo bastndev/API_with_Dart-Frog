ca6b532970d018ec453fcba831e89312bca7df1f
bastndev <gohit24@gmail.com>
2024-01-06 15:45:44 -0500

    :arrow_up: Update `RECENT`

---
+++ routes/pets/ws/adopted.dart
@@ -0,0 +1,9 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  final handler = webSocketHandler((channel, protocol) {
+    // TODO: react to new connections.
+  });
+  return handler(context);
+}
